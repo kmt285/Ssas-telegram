4fa04b83dd3e201e0e2ffcc1731a7d0eaaa0ec8d
kmt285 <248855908+kmt285@users.noreply.github.com>
2026-06-03 13:31:22 +0630

    Create database.py

---
+++ core/database.py
@@ -0,0 +1,14 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from core.config import MONGODB_URI, DB_NAME
+
+class Database:
+    def __init__(self):
+        self.client = AsyncIOMotorClient(MONGODB_URI)
+        self.db = self.client[DB_NAME]
+        
+        self.businesses = self.db.businesses
+        self.services = self.db.services
+        self.subscriptions = self.db.subscriptions
+        self.users = self.db.users
+
+db = Database()
